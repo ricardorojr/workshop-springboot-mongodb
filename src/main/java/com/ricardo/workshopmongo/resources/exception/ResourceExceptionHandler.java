eef3675e9f87dc16a5b2ed454eb716f03331ac9a
Ricardo Rodrigues <ricardorodriguesjr87@gmail.com>
2020-04-16 16:47:30 -0300

    inserindo controlleradvice

--- src/main/java/com/ricardo/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/ricardo/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.ricardo.workshopmongo.services.exception.ObjectNotFoundException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
