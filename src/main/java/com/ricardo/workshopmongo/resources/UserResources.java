cef9175434c3ee2a63b6171cf6d56812884d5428
Ricardo Rodrigues <ricardorodriguesjr87@gmail.com>
2020-04-27 21:21:13 -0300

    delecao de usuario por id, com delete

--- src/main/java/com/ricardo/workshopmongo/resources/UserResources.java
+++ src/main/java/com/ricardo/workshopmongo/resources/UserResources.java
@@ -18,30 +18,36 @@ import com.ricardo.workshopmongo.dto.UserDTO;
 import com.ricardo.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResources {
 
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	@RequestMapping(value= "/{id}",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
+
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/ricardo/workshopmongo/services/UserService.java
+++ src/main/java/com/ricardo/workshopmongo/services/UserService.java
@@ -13,23 +13,29 @@ import com.ricardo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll()	{
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(" Objeto não encontrado "));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
-	 public User findById(String id) {
-			  Optional<User>  obj = repo.findById(id);
-			return obj . orElseThrow (() -> new  ObjectNotFoundException ( " Objeto não encontrado " ));
-	}	
-	 
-	 public User insert(User obj) {
-		 return repo.insert(obj);
-	 }
-	 public User fromDTO(UserDTO objDto) {
-		 return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-	 }
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
